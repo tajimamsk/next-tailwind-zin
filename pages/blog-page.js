4a1b620cb96340761159a72a87a5d275baaef5de
tajimamsk <masaki.tajima@g-seed.co.jp>
2024-05-23 14:41:36 +0900

    layout

---
+++ pages/blog-page.js
@@ -0,0 +1,8 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+const Blog = () => {
+  return <Layout title="Blog">blog page</Layout>;
+};
+
+export default Blog;
---
+++ pages/contact-page.js
@@ -0,0 +1,8 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+const contact = () => {
+  return <Layout title="contact">contact</Layout>;
+};
+
+export default contact;
